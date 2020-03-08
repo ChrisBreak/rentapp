ff9dd8a06dc537dc082cbd8d139dfcc9a81ce869
Cristhian <csotelo88@hotmail.com>
2020-03-07 22:41:25 -0700

    pagination done

--- rentApp.js
+++ rentApp.js
@@ -1,6 +1,41 @@
+let pages = ["proInfo", "purchInfo", "rentInfo"];
+let nextButton, backButton, currentP;
+
+function changePage(page) {
+  for(var i = 0; i < pages.length; i++) {
+    document.getElementById(pages[i]).style.display = "none";
+  }
+  document.getElementById(page).style.display = "block";
+  currentP = page;
+}
 
 
 window.onload = function() {
+  nextButton = document.getElementById('nextB');
+  backButton = document.getElementById('backB');
+  changePage("proInfo");
+  backButton.style.display = "none";
 
-  
+  nextButton.onclick = function() {
+    if (currentP === "proInfo") {
+      changePage("purchInfo");
+      backButton.style.display = "inline-block";
+    }
+    else if (currentP === "purchInfo") {
+      changePage("rentInfo");
+      nextButton.style.display = "none";
+    }
+  }
+
+  backButton.onclick = function() {
+    if (currentP === "rentInfo") {
+      changePage("purchInfo");
+      nextButton.style.display = "inline-block";
+    }
+    else if (currentP === "purchInfo") {
+      changePage("proInfo");
+      backButton.style.display = "none";
+    }
+  }
+
 };
