dc34f71bf2441f86db75f7982f1a2ea95f5897a0
Cristhian <csotelo88@hotmail.com>
2020-03-08 16:25:32 -0600

    finished progress bar

--- rentApp.js
+++ rentApp.js
@@ -1,5 +1,5 @@
 let pages = ["proInfo", "purchInfo", "rentInfo", "results"];
-let nextButton, backButton, currentP;
+let nextButton, backButton, currentP, proBar;
 
 function changePage(page) {
   for(var i = 0; i < pages.length; i++) {
@@ -7,10 +7,16 @@ function changePage(page) {
   }
   document.getElementById(page).style.display = "block";
   currentP = page;
+  document.getElementById(page+"Bar").style.color = "#005ce6";
+  if (page === "proInfo") proBar.value = 12;
+  else if (page === "purchInfo") proBar.value = 40;
+  else if (page === "rentInfo") proBar.value = 68;
+  else proBar.value = 100;
 }
 
 
 window.onload = function() {
+  proBar = document.getElementById('progressBar');
   nextButton = document.getElementById('nextB');
   backButton = document.getElementById('backB');
   changePage("proInfo");
@@ -34,13 +40,16 @@ window.onload = function() {
     if (currentP === "results") {
       changePage("rentInfo");
       nextButton.style.display = "inline-block";
+      document.getElementById("resultsBar").style.color = "#696969";
     }
     else if (currentP === "rentInfo") {
       changePage("purchInfo");
+      document.getElementById("rentInfoBar").style.color = "#696969";
     }
     else if (currentP === "purchInfo") {
       changePage("proInfo");
       backButton.style.display = "none";
+      document.getElementById("purchInfoBar").style.color = "#696969";
     }
   }
 
